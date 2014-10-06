c0dbba61b21af30beab9361dab7358f46150093e
GitLab <gitlab-test@intra.sapo.pt>
2014-10-06 16:38:45 +0000

    Shibboleth SSO integration.
    With Shibboleth enabled, the login screen in Gitlab is hidden.
    Session is started automatically because the user can only access the site after successful login on Single Sign On.
    
    Hide HTTP repository clone URL, when the application is behind Shibboleth access control, the HTTP Basic Auth will not work and therefore HTTP access to repositories is not authenticable.

--- app/helpers/oauth_helper.rb
+++ app/helpers/oauth_helper.rb
@@ -3,6 +3,10 @@ module OauthHelper
     Devise.omniauth_providers.include?(:ldap)
   end
 
+  def shibboleth_enabled?
+    Devise.omniauth_providers.include?(:shibboleth)
+  end
+
   def default_providers
     [:twitter, :github, :google_oauth2, :ldap]
   end
